9d5fb2b4c8313015e8d408b12e46e256f1456215
lemus <lemus777@gmail.com>
2023-04-19 14:02:58 +0200

    uso de estado

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -4,11 +4,12 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 export function App () {
   return (
     <section className='App'>
-      <TwitterFollowCard isFollowing username='lemus777' name='Jorge Lemus' /> {/* si usamos un prop sin declarar lo interpreta como true */}
-      <TwitterFollowCard isFollowing={false} username='midudev' name='Miguel Ángel Durán' /> {/* para false si hay que declararlo de este modo */}
-      <TwitterFollowCard isFollowing username='pheralb' name='Pablo Hernández' />
-      <TwitterFollowCard isFollowing username='elonmusk' name='Elon Musk' />
-      <TwitterFollowCard isFollowing username='vnxder' name='Vanderhart' />
+      <TwitterFollowCard userName='lemus777'> {/* si usamos un prop sin declarar lo interpreta como true */}
+        Jorge Lemus
+      </TwitterFollowCard>
+      <TwitterFollowCard userName='midudev'> {/* para false si hay que declararlo de este modo */}
+      Miguel Ángel Durán
+      </TwitterFollowCard>
     </section>
   )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,23 +1,36 @@
-export function TwitterFollowCard ({ username, name, isFollowing}) {
-  const addAt = (username) => `@${username}`
+import { useState } from "react"
+
+export function TwitterFollowCard ({ children, userName}) {
+  const [isFollowing, setIsFollowing] = useState(false)
+
+  const text = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
 
   return (
     <article className='tw-followCard'>
       <header className='tw-followCard-header'>
         <img
           className='tw-followCard-avatar'
-          alt={`el avatar de ${username}`}
-          src={`https://unavatar.io/${username}`}
+          alt={`el avatar de ${userName}`}
+          src={`https://unavatar.io/${userName}`}
         />
         <div className='tw-followCard-info'>
-          <strong>{name}</strong>
-          <span className='tw-followCard-infoUsername'>{addAt(username)}</span>
+          <strong>{children}</strong>
+          <span className='tw-followCard-infoUsername'>
+            @{userName}
+          </span>
         </div>
       </header>
 
       <aside>
-        <button className='tw-followCard-button'>
-          Seguir
+        <button className={buttonClassName} onClick={handleClick}>
+          {text}
         </button>
       </aside>
     </article>
