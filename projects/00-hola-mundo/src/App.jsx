c9b759fd9170fe2a2882648ba0473f6e8c4c085e
lemus <lemus777@gmail.com>
2023-04-20 12:38:23 +0200

    renderizado de listas

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,15 +1,43 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+  {
+    userName: 'lemus777',
+    name: 'Jorge Lemus',
+    isFollowing: true
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: false
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Heraldo',
+    isFollowing: false
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomás',
+    isFollowing: true
+  }
+]
+
 export function App () {
   return (
     <section className='App'>
-      <TwitterFollowCard userName='lemus777'> {/* si usamos un prop sin declarar lo interpreta como true */}
-        Jorge Lemus
-      </TwitterFollowCard>
-      <TwitterFollowCard userName='midudev'> {/* para false si hay que declararlo de este modo */}
-      Miguel Ángel Durán
-      </TwitterFollowCard>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+        ))
+      }
     </section>
   )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
-export function TwitterFollowCard ({ children, userName}) {
-  const [isFollowing, setIsFollowing] = useState(false)
+export function TwitterFollowCard ({ children, userName, initialIsFollowing}) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
   const text = isFollowing ? 'Siguiendo' : 'Seguir'
   const buttonClassName = isFollowing
@@ -30,7 +30,8 @@ export function TwitterFollowCard ({ children, userName}) {
 
       <aside>
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
         </button>
       </aside>
     </article>
